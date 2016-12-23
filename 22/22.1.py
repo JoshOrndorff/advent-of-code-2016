b6a6b0b9ba110a58483d6e91c6351ce68a30afde
Josh Orndorff <admin@joshorndorff.com>
2016-12-22 19:23:36 -0500

    Solved 22.1. Second part sounds tough.

---
+++ 22/22.1.py
@@ -0,0 +1,42 @@
+
+# First parse the input into a list
+nodes = []
+with open("input.txt", 'r') as f:
+  # Skip the first two lines because they are headers
+  f.next()
+  f.next()
+  for line in f:
+    # Only keep name, used, avail
+    nodes.append((line[15:22], int(line[30:33]), int(line[37:40])))
+
+# A little info just to help understand the data
+n = len(nodes)
+print("Number of nodes is: {}".format(n))
+print("Number of nodes squared: {}".format(n * n))
+print("Total number of pairs: {}".format(n * (n - 1) / 2))
+
+# Loop through each potential pair of nodes noting viable ones.
+numViable = 0
+for a in nodes:
+  
+  #print("Current a node: {}".format(str(a)))
+  # If a is empty the pair is not vialbe
+  if a[1] == 0:
+    print("Rejecting a as empty. " + str(a))
+    continue
+  
+  for b in nodes:
+    
+    # If this is the same node as a, it isn't really even a pair
+    if b[0] == a[0]:
+      continue
+    
+    # If the the data on a would fit on b, the pair is viable
+    if b[2] >= a[1]:
+      numViable += 1
+
+# Print the final result
+print(numViable)
+
+# 1854 is too high # Got this when I was parsing names incorrectly
+# 1911 is probably also too high, right?
